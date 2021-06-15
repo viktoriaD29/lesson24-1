bcf141a06a0dd30f42d47d63c56a03e33a45b197
Viktoria <doduchenko29@gmail.com>
2021-06-15 19:57:20 +0300

    add

--- index.js
+++ index.js
@@ -6,14 +6,14 @@
 //2. додаємо цей день тижня до days
 //3. виводимо відповідний елемент по індексу
 
-export const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
   const day = new Date(date).getDate();
 
   const dateInFuture = new Date(date).setDate(day + days);
 
   return weekDays[new Date(dateInFuture).getDay()];
 };
-/*const result = dayOfWeek(new Date(2019, 0, 1), 14);
-console.log(result);*/
+const result = dayOfWeek(new Date(2019, 0, 1), 14);
+console.log(result);
