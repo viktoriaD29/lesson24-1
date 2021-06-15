d4e42014ecd16c4c201241f288c538ec9a9c2ee7
Viktoria <doduchenko29@gmail.com>
2021-06-15 19:54:43 +0300

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+//input: obj, number
+//output: string
+
+//algo:
+//1. зчитуєм яка дата в date
+//2. додаємо цей день тижня до days
+//3. виводимо відповідний елемент по індексу
+
+const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
+
+const dayOfWeek = (date, days) => {
+  const day = new Date(date).getDate();
+
+  const dateInFuture = new Date(date).setDate(day + days);
+
+  return weekDays[new Date(dateInFuture).getDay()];
+};
+const result = dayOfWeek(new Date(2019, 0, 1), 14);
+console.log(result);
